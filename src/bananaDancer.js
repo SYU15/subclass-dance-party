c2856222b5b2cb6b999259ee03dac33ac5c0f606
SYU88 <sarah.m.yu88@gmail.com>
2015-05-20 20:22:31 -0700

    Added background and positioned gif!

--- src/bananaDancer.js
+++ src/bananaDancer.js
@@ -1,6 +1,7 @@
 var BananaDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<iframe src="http://giphy.com/embed/PqRbPcAt1cnHG" width="80" height="53" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
+  this.$node = $('<div class="gif"><iframe src="http://giphy.com/embed/PqRbPcAt1cnHG" width="80" height="53" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>');
+  this.$node.css({top: this.top, left: this.left});
 }
 
 BananaDancer.prototype = Object.create(Dancer.prototype);
